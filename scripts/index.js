8618160063d3a85e276d4c30776b9555e1b633b1
solidity12 <volibear1001@gmail.com>
2023-08-14 00:31:42 +0900

    build: Updated index file

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,11 @@
-const modeCcipSync = require("./mode_ccip");
-const modeHyperlaneSync = require("./mode_hyperlane");
-const zoraCcipSync = require("./zora_ccip");
-const zoraHyperlaneSync = require("./zora_hyperlane");
-const opCcipSync = require("./op_ccip");
-const opHyperlaneSync = require("./op_hyperlane");
-const baseCcipSync = require("./base_ccip");
-const baseHyperlaneSync = require("./base_hyperlane");
+const storeDB = require("./storeDB");
+const storeDBMockup = require("./storeDBMockup");
+const storeDBTheGraph = require("./storeDBTheGraph");
 
 async function start() {
-    //mode
-    modeCcipSync();
-    modeHyperlaneSync();
-    //zora
-    zoraCcipSync();
-    zoraHyperlaneSync();
-    //op
-    opCcipSync();
-    opHyperlaneSync();
-    //base
-    baseCcipSync();
-    baseHyperlaneSync();
+    storeDB();
+    storeDBMockup();
+    storeDBTheGraph();
 }
 
 start();
